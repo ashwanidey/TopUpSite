8e8e6d318f407f2de5c9d1ae7b5c722725c52282
abdulrakib <firefalls2004@gmail.com>
2024-06-14 12:00:32 +0530

    host-onrender

--- frontend/src/context/VariableContext.jsx
+++ frontend/src/context/VariableContext.jsx
@@ -3,9 +3,9 @@ import { json } from "react-router-dom";
 
 export const VariableContext= createContext({});
 export const VariableProvider = ({children}) => {
-  //const host = "https://topupsite.onrender.com";
+  const host = "https://topupsite.onrender.com";
   // const host = "http://localhost:3001";
-const host = "http://3.6.39.63:3001"
+//const host = "http://3.6.39.63:3001"
   const imageUrl = "../../assets/"
   const mlbb = "666a62769f96a788ccdc9820";
   const mlbbph = "666a62769f96a788ccdc982c";
