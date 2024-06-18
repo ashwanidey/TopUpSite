5d14d041f71a2f271cf5624a4faa4951d79acefc
abdulrakib <firefalls2004@gmail.com>
2024-06-18 22:15:21 +0530

    host onrender

--- frontend/src/context/VariableContext.jsx
+++ frontend/src/context/VariableContext.jsx
@@ -3,8 +3,8 @@ import { json } from "react-router-dom";
 
 export const VariableContext= createContext({});
 export const VariableProvider = ({children}) => {
-  const host = "http://localhost:3001";
-  // const host = "https://mirakistore.com";
+  //const host = "http://localhost:3001";
+  // const host = "https://topupsite onrender.com";
   const imageUrl = "../../assets/"
   const mlbb = "100";
   const mlbbph = "112";
